689cbb32ce3a65115df2b4364486ea55a9ef44b0
tusharbisen <tusharbisen32Gmail.com>
2025-07-29 16:18:19 +0530

    New Video Added

--- components/video-section/video-intro-section.tsx
+++ components/video-section/video-intro-section.tsx
@@ -58,7 +58,7 @@ export default function VideoIntroSection() {
               onEnded={() => setIsPlaying(false)}
             >
               <source
-                src="https://res.cloudinary.com/dy1w6zqom/video/upload/v1751344140/_%E0%A4%97%E0%A4%BF%E0%A4%A7%E0%A4%BE%E0%A4%A1%E0%A5%80_%E0%A4%97%E0%A5%8D%E0%A4%B0%E0%A4%BE%E0%A4%AE_%E0%A4%95%E0%A4%A8%E0%A5%87%E0%A4%95%E0%A5%8D%E0%A4%9F_%E0%A4%97%E0%A4%BE%E0%A4%81%E0%A4%B5_%E0%A4%95%E0%A5%80_%E0%A4%86%E0%A4%B5%E0%A4%BE%E0%A4%9C%E0%A4%BC_%E0%A4%B9%E0%A4%B0_%E0%A4%A6%E0%A4%BF%E0%A4%B2_%E0%A4%95%E0%A5%87_%E0%A4%B8%E0%A4%BE%E0%A4%A5__bk7uu1.mp4"
+                src="https://res.cloudinary.com/dy1w6zqom/video/upload/v1753786003/Gidhadi_Gram_connect_1_sbwhpq.mp4"
                 type="video/mp4"
               />
               <source src="/videos/gidhadi-gram-intro.webm" type="video/webm" />
